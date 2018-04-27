a55f7994db7f24f2a5eeaa1428301392216fc5fd
Alex Bannon <alexbannon@gmail.com>
2018-04-27 12:29:07 -0400

    bug fixes

--- app/controllers/birthday_controller.rb
+++ app/controllers/birthday_controller.rb
@@ -1,18 +1,26 @@
 class BirthdayController < ApplicationController
 
   def show_db
+    offset = params[:offset]
+    offset ||= 0
     @celebrities = Celebrity.where(
       birth_month: params[:month],
       birth_day: params[:day]
-    )
-    render "show"
+    ).limit(50).offset(offset)
 
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render :json => @celebrities.as_json(:include_media => true) }
+    end
   end
 
   def show_scrape
     month = params[:month]
     day = params[:day]
-    offset = Integer(params[:offset])
+    offset = params[:offset]
+    if offset
+      offset = Integer(offet)
+    end
 
     scraper = Scraper.new
     scraper.process_one_date(month, day, offset)
